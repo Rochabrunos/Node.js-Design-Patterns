080611ac37dd90dd1bb5e2219c40f1172c7d5ec8
Bruno Rocha <bsrocha1005@gmail.com>
2025-06-04 18:21:17 -0300

    Creating a new reference for readFile function in main.js, breaking the code

--- Chapter 02/ESM/xxxxxxx - Modifying Other Modules/main.js
+++ Chapter 02/ESM/xxxxxxx - Modifying Other Modules/main.js
@@ -1,9 +1,15 @@
-import fs from 'fs'
+// Note the use of default import
+// import fs from 'fs'
+// Change to named import will break the code
+// Because we not have access to readFile live-bind to modify it
+// Will throw an error, since fake-path does not exist
+import { readFile } from 'fs'
+
 import { mockEnable, mockDisable } from './mock-read-file.js'
 
 mockEnable(Buffer.from('Hello World'))
 
-fs.readFile('fake-path', (err, data) => {
+readFile('fake-path', (err, data) => {
     if (err) {
         console.errorr(err)
         process.exit(1)
