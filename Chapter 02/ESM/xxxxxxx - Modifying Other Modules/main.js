e0904f8d9b7fbc0c04ffec538402ac262ffde57d
Bruno Rocha <bsrocha1005@gmail.com>
2025-06-04 16:24:09 -0300

    Changing the fs.readFile implementation in runtime

---
+++ Chapter 02/ESM/xxxxxxx - Modifying Other Modules/main.js
@@ -0,0 +1,14 @@
+import fs from 'fs'
+import { mockEnable, mockDisable } from './mock-read-file.js'
+
+mockEnable(Buffer.from('Hello World'))
+
+fs.readFile('fake-path', (err, data) => {
+    if (err) {
+        console.errorr(err)
+        process.exit(1)
+    }
+    console.log(data.toString()) // --> Hello World
+})
+
+mockDisable()
