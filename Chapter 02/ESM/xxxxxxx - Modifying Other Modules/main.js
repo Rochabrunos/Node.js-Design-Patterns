8b37ff44acf1ec534c3c79578bc4ee10e6f5de06
Bruno Rocha <bsrocha1005@gmail.com>
2025-06-04 18:25:03 -0300

    Fixing the code adding syncBuiltinESMExports to import the reference to readFile

--- Chapter 02/ESM/xxxxxxx - Modifying Other Modules/main.js
+++ Chapter 02/ESM/xxxxxxx - Modifying Other Modules/main.js
@@ -4,10 +4,14 @@
 // Because we not have access to readFile live-bind to modify it
 // Will throw an error, since fake-path does not exist
 import { readFile } from 'fs'
+// To fix this, we can use syncBuiltinESMExport to import the readFile function again
+import { syncBuiltinESMExports } from 'module'
 
 import { mockEnable, mockDisable } from './mock-read-file.js'
 
 mockEnable(Buffer.from('Hello World'))
+// After modify the readFile function, we can use it
+syncBuiltinESMExports()
 
 readFile('fake-path', (err, data) => {
     if (err) {
