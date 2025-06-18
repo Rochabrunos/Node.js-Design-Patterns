5855ea575b25ff41eb218ebab8b62116b6fc59ae
Bruno Rocha <bsrocha1005@gmail.com>
2025-06-18 14:37:27 -0300

    Fixing the code by making the call to find function after we subscribe to the events

--- Chapter 03/05 - Observable Pattern/03 - Synchronous and Asynchronous Events/main.js
+++ Chapter 03/05 - Observable Pattern/03 - Synchronous and Asynchronous Events/main.js
@@ -6,23 +6,27 @@ const findRegexInstance = new FindRegex(/Hello/)
  * In this example we subscribe to the events after we call the `find`method.
  *  This works only because we use asynchronous events.
  * */
-findRegexInstance
-    .addFile('fileA.txt')
-    .addFile('fileB.txt')
-    .find()
-    .on('fileread', file => console.log(`${file} was read1`))
-    .on('found', (file, match) => console.log(`Matched1 "${match}" in ${file}`))
-    .on('error', err => console.error(`Error emitter1 ${err.message}`)) 
-
-// Let's change the code to make it synchronous and do the same call
-// It won't work because now the events are ommited in the same tick of 
-// the event loop before we subscribe to them
-
 // findRegexInstance
 //     .addFile('fileA.txt')
 //     .addFile('fileB.txt')
 //     .find()
 //     .on('fileread', file => console.log(`${file} was read`))
-//     .on('found', (file, match) => console.log(`Matched "${match}" in ${file}`))
+//     .on('found', (file, match) => console.log(`Matched1 "${match}" in ${file}`))
 //     .on('error', err => console.error(`Error emitter ${err.message}`)) 
+
+// Let's change the code to make it synchronous and do the same call
+// It won't work because now the events are ommited in the same tick of 
+// the event loop before we subscribe to them
+
+
+// Solution is to make the call to `find` method after we subscribe to the events
+findRegexInstance
+    .addFile('fileA.txt')
+    .addFile('fileB.txt')
+    .on('fileread', file => console.log(`${file} was read`))
+    .on('found', (file, match) => console.log(`Matched "${match}" in ${file}`))
+    .on('error', err => console.error(`Error emitter ${err.message}`)) 
+    .find()
+
+
     
