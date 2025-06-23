6f1cea575d3bd001f08362b66b21119ebf07c0ca
Bruno Rocha <bsrocha1005@gmail.com>
2025-06-23 16:14:11 -0300

    Exercise 2: a simple modification - i.e. emit a tick event after the funtion is invoked

--- Chapter 03/06 - Exercises/6.3 - Ticker, a simple motification/main.js
+++ Chapter 03/06 - Exercises/6.3 - Ticker, a simple motification/main.js
@@ -1,17 +1,14 @@
 import { EventEmitter } from 'events'
 
 /**
- * Ticker: Write a function that accepts a number and a callback as the arguments. 
- * The function will return an EventEmitter that emits an event called tick 
- * every 50 milliseconds until the number of milliseconds is passed from the 
- * invocation of the function. The function will also call the callback when 
- * the number of milliseconds has passed, providing, as the result, the total 
- * count of tick events emitted. Hint: you can use setTimeout() to schedule another setTimeout() recursively.
+ * A simple modification: Modify the function created in exercise 3.2 so that it 
+ * emits a tick event immediately after the function is invoked.
  */
 
 let count = 0
 function ticker(ms, cb) {
     const emitter = new EventEmitter()
+    process.nextTick(() => {emitter.emit('tick'); count++})
     const interval = setInterval(() => {
         emitter.emit('tick')
         count++
