c12bc76236c62c24401841f792a92a2be12c707e
Bruno Rocha <bsrocha1005@gmail.com>
2025-06-23 16:06:36 -0300

    Adding a tick counter

--- Chapter 03/06 - Exercises/6.2 - Ticker/main.js
+++ Chapter 03/06 - Exercises/6.2 - Ticker/main.js
@@ -1,14 +1,25 @@
 import { EventEmitter } from 'events'
 
+/**
+ * Ticker: Write a function that accepts a number and a callback as the arguments. 
+ * The function will return an EventEmitter that emits an event called tick 
+ * every 50 milliseconds until the number of milliseconds is passed from the 
+ * invocation of the function. The function will also call the callback when 
+ * the number of milliseconds has passed, providing, as the result, the total 
+ * count of tick events emitted. Hint: you can use setTimeout() to schedule another setTimeout() recursively.
+ */
+
+let count = 0
 function ticker(ms, cb) {
     const emitter = new EventEmitter()
     const interval = setInterval(() => {
         emitter.emit('tick')
-        
+        count++
     }, 50)
     setTimeout(() => {
         clearInterval(interval)
         emitter.emit('complete')
+        console.log(`Total ticks: ${count}`)
         cb()
     }, ms)
   
